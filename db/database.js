97dfec98529110bbb1cfbcdb8f54c9cb20e06174
Michael Nabatov <happyanddebtfree@gmail.com>
2022-10-31 22:36:40 -0400

    Created db directory, schema.sql, and seeds.sql files.

--- db/database.js
+++ db/database.js
@@ -23,7 +23,7 @@ module.exports = {
     dbExecute(`INSERT INTO roles (title, salary, department_id) 
                VALUES ('${title}', ${salary}, ${department_id});`),
 
-  addEmployee: ({title, salary, department_id}) => 
-    dbExecute(`INSERT INTO roles (title, salary, department_id) 
-               VALUES ('${title}', ${salary}, ${department_id});`),
+  addEmployee: ({first_name, last_name, role_id, manager_id}) => 
+    dbExecute(`INSERT INTO roles (first_name, last_name, role_id, manager_id) 
+               VALUES ('${first_name}', '${last_name}', ${role_id}, ${manager_id});`),
 }
